89796d184b556baa4e212119cc2d576145b13bd6
Sandeep Mishra <35232211+sandeepkumarmishra354@users.noreply.github.com>
2018-03-24 22:43:25 +0530

    Update AppLogic.js

--- qml/AppLogic.js
+++ qml/AppLogic.js
@@ -1,23 +1,77 @@
-function playThis(index, songName, row)
+function playThis(index, row)
 {
     setPlayingIcon()
     songRow = row
-    batplayer.source = mainplaylist.itemSource(index)
+    mainplaylist.currentIndex = index
     batplayer.play()
-    currentSongName = songName
-    playingpage.title = songName
-    currentSongIndex = index
 }
 
 function setPlayingIcon()
 {
-    if(songRow != undefined)
+    if(songRow !== undefined)
     {
         songRow.iconSource = IconType.music
     }
 }
 
-function currentRow(curindex)
+function playPrevTrack()
 {
-    return mainPage.returnRow(curindex)
+    mainplaylist.previous()
+}
+
+function playNextTrack()
+{
+    mainplaylist.next()
+}
+
+function playOrPause()
+{
+    if(batplayer.playbackState === Audio.PlayingState)
+        batplayer.pause()
+    else
+        batplayer.play()
+}
+
+function calculateRemTime(position)
+{
+    var totalDurSec = Math.floor(position / 1000)
+    var min = Math.floor(totalDurSec/60)
+    if(min < 10)
+        min = "0"+min
+    var sec = Math.floor(totalDurSec%60)
+    if(sec < 10)
+        sec = "0"+sec
+    var time = min + ":" + sec
+    playingpage.remTime = time
+    if(playingpage.remTime === playingpage.totalTime)
+    {
+        console.log("Song finished")
+        playingpage.totalTime = "0:0"
+        playingpage.remTime = "0:0"
+        LOGIC.playNextTrack()
+        playingpage.title = currentSongName
+        batplayer.play()
+    }
+}
+
+function calculateTotalTime(duration)
+{
+    var totalDurSec = Math.floor(duration / 1000)
+    var min = Math.floor(totalDurSec/60)
+    if(min < 10)
+        min = "0"+min
+    var sec = Math.floor(totalDurSec%60)
+    if(sec < 10)
+        sec = "0"+sec
+    var time = min + ":" + sec
+    playingpage.totalTime = time
+}
+
+function songChanged(src)
+{
+    var srcstr = src.toString()
+    var li = srcstr.lastIndexOf("/")
+    var mp3 = srcstr.slice(++li);
+    currentSongName = mp3
+    playingpage.songtitle = mp3
 }
