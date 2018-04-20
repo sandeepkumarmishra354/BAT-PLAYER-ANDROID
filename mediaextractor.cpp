bc5338c5cba8f64d3430c160810fb8aebdcb4988
Sandeep Mishra <35232211+sandeepkumarmishra354@users.noreply.github.com>
2018-04-20 11:52:43 +0530

    Add files via upload

--- mediaextractor.cpp
+++ mediaextractor.cpp
@@ -1,25 +1,44 @@
 #include <QDebug>
 #include <QImage>
+#include <QFile>
+#include <QDir>
+#include <QIODevice>
+#include <QTextStream>
 #include "mediaextractor.h"
 
+// Constructor
 MediaExtractor::MediaExtractor(QObject *parent) : QObject(parent)
 {
-    // constructor
+    #ifdef Q_OS_LINUX
+        cvrArtPath = linuxPath;
+        svPath = cvrArtPath;
+        QDir folder(cvrArtPath);
+        if(!folder.exists())
+            folder.mkdir(cvrArtPath);
+    #endif
+
+    #ifdef Q_OS_ANDROID
+        QUrl url(androidPathCoverArtList);
+        cvrArtPath = url.toLocalFile();
+        QDir fldr(cvrArtPath);
+        if(!fldr.exists())
+            fldr.mkdir(cvrArtPath);
+        url = androidPathCoverArt;
+        cvrArtPath = url.toLocalFile();
+        fldr.setPath(cvrArtPath);
+        if(!fldr.exists())
+            fldr.mkdir(cvrArtPath);
+        svPath = cvrArtPath;
+    #endif
+
+    initCvrArtList(); // initializes list of cover arts
 }
 
-void MediaExtractor::mediaMetaData(QString mediaPath)
+void MediaExtractor::initCvrArtList()
 {
-    QUrl mediaURL(mediaPath);
-    QString localFile = mediaURL.toLocalFile();
-    TagLib::FileRef mediaFile(localFile.toStdString().c_str());
-    if(!mediaFile.isNull() && mediaFile.tag())
-    {
-        TagLib::Tag *mediaTag = mediaFile.tag();
-        qDebug()<<mediaTag->album().toCString();
-        qDebug()<<mediaTag->artist().toCString();
-        qDebug()<<mediaTag->genre().toCString();
-        qDebug()<<mediaTag->title().toCString();
-    }
+    QDir dir(cvrArtPath);
+    cvrArtListInfo.clear();
+    cvrArtListInfo = dir.entryInfoList(QDir::Files);
 }
 
 QString MediaExtractor::getTitle(QString mediaPath)
@@ -34,7 +53,6 @@ QString MediaExtractor::getTitle(QString mediaPath)
         title = mediaTag->title().toCString();
         if(title.isEmpty() | title.isNull())
             title = mediaURL.fileName();
-        qDebug()<<title;
     }
     return title;
 }
@@ -51,12 +69,104 @@ QString MediaExtractor::getArtist(QString mediaPath)
         artist = mediaTag->artist().toCString();
         if(artist.isEmpty() | artist.isNull())
             artist = "unknown";
-        qDebug()<<artist;
     }
     return artist;
 }
 
-void MediaExtractor::getAlbumCover(QString mediaPath)
+void MediaExtractor::extractAlbumCover(QString mediaPath)
 {
+    static const char *IdPicture = "APIC";
+    bool already = false;
+    QString title;
+    QUrl mediaURL(mediaPath);
+    QString localFile = mediaURL.toLocalFile();
 
+    TagLib::FileRef mediaFile(localFile.toStdString().c_str());
+    if(!mediaFile.isNull() && mediaFile.tag())
+    {
+        TagLib::Tag *mediaTag = mediaFile.tag();
+        title = mediaTag->title().toCString();
+        if(title.isEmpty() | title.isNull())
+            title = mediaURL.fileName();
+        int index = title.lastIndexOf(".");
+        title = title.mid(0,index);
+        title.prepend(svPath+"/");
+        foreach(QFileInfo itm, cvrArtListInfo)
+        {
+            if(itm.absoluteFilePath() == title)
+            {
+                already = true;
+                break;
+            }
+        }
+    }
+
+    if(!already)
+    {
+        TagLib::MPEG::File mpegFile(localFile.toStdString().c_str());
+        TagLib::ID3v2::Tag *id3v2tag = mpegFile.ID3v2Tag();
+        TagLib::ID3v2::FrameList Frame;
+        TagLib::ID3v2::AttachedPictureFrame *PicFrame;
+        void *SrcImage;
+        unsigned long Size;
+
+        if ( id3v2tag )
+        {
+            Frame = id3v2tag->frameListMap()[IdPicture];
+            if (!Frame.isEmpty() )
+            {
+                for(TagLib::ID3v2::FrameList::ConstIterator it = Frame.begin(); it != Frame.end(); ++it)
+                {
+                    PicFrame = (TagLib::ID3v2::AttachedPictureFrame *)(*it);
+                    if ( PicFrame->type() == TagLib::ID3v2::AttachedPictureFrame::FrontCover)
+                    {
+                        Size = PicFrame->picture().size();
+                        SrcImage = malloc ( Size );
+                        if ( SrcImage )
+                        {
+                            FILE *jpegFile;
+                            jpegFile = fopen(title.toStdString().c_str(),"wb");
+
+                            memcpy ( SrcImage, PicFrame->picture().data(), Size );
+                            fwrite(SrcImage,Size,1, jpegFile);
+                            fclose(jpegFile);
+                            free( SrcImage );
+                            //mediaCoverArtPathList.append(title);
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+
+    initCvrArtList();
+}
+
+QString MediaExtractor::getAlbumCover(QString mediaTitle)
+{
+    QString path;
+    #ifdef Q_OS_LINUX
+        path = linuxPath+"/";
+    #endif
+    #ifdef Q_OS_ANDROID
+        path = androidPathCoverArt+"/";
+    #endif
+    QString cvrPath = "";
+    int index = mediaTitle.lastIndexOf(".");
+    mediaTitle = mediaTitle.mid(0,index);
+    foreach(QFileInfo itm, cvrArtListInfo)
+    {
+        if(itm.fileName().contains(mediaTitle))
+        {
+            cvrPath = mediaTitle.prepend(path);
+            break;
+        }
+    }
+    return cvrPath;
+}
+
+MediaExtractor::~MediaExtractor()
+{
+    qDebug()<<"C++ Destructor";
 }
--- mediaextractor.h
+++ mediaextractor.h
@@ -3,29 +3,47 @@
 
 #include <QObject>
 #include <QString>
+#include <QStringList>
 #include <QUrl>
+#include <QFileInfoList>
 #include <iostream>
 #include <iomanip>
 #include <string>
 #include <stdio.h>
-
+#include <cstdio>
 #include <taglib/fileref.h>
 #include <taglib/tag.h>
 #include <taglib/tpropertymap.h>
-#include <taglib/attachedpictureframe.h>
 #include <taglib/id3v1tag.h>
+#include <taglib/id3v2tag.h>
+#include <taglib/mpegfile.h>
+#include <taglib/id3v2frame.h>
+#include <taglib/id3v2header.h>
+#include <taglib/attachedpictureframe.h>
 using namespace TagLib;
 using namespace std;
 
 class MediaExtractor : public QObject
 {
     Q_OBJECT
+private:
+    //QStringList mediaCoverArtPathList;
+    QFileInfoList cvrArtListInfo;
+    const QString androidPathCoverArtList = "file:///sdcard/.BATPLAYER";
+    const QString androidPathCoverArt = "file:///sdcard/.BATPLAYER/.CoverArt";
+    const QString linuxPath = "/root/CoverArt";
+    QString pathToList;
+    QString cvrArtPath;
+    QString svPath;
 public:
     explicit MediaExtractor(QObject *parent = nullptr);
-    Q_INVOKABLE void mediaMetaData(QString mediaPath);
+    ~MediaExtractor();
     Q_INVOKABLE QString getTitle(QString mediaPath);
     Q_INVOKABLE QString getArtist(QString mediaPath);
-    Q_INVOKABLE void getAlbumCover(QString mediaPath);
+    Q_INVOKABLE void extractAlbumCover(QString mediaPath);
+    Q_INVOKABLE QString getAlbumCover(QString mediaTitle);
+private slots:
+    void initCvrArtList();
 };
 
 #endif // MEDIA_EXTRACTOR_H
